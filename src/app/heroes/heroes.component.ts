b437e5b8e3864852d0aa491a4824ee04e8bf3341
alexandre-w <alexandre.git67@gmail.com>
2018-05-19 15:17:03 +1000

    last update of the UI

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
 
   constructor(private heroService: HeroService) { }
@@ -25,8 +24,18 @@ export class HeroesComponent implements OnInit {
                     .subscribe(heroes => this.heroes = heroes)
   }
 
-  onSelect(hero: Hero){
-    this.selectedHero = hero ;
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.addHero( {name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
